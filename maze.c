cf0b2f4de23f0e3f2850738a66f3ae75812d0e50
Rui-Jie Fang <rui.jie.fang@gmail.com>
2019-03-19 16:14:21 -0400

    Add windowing functions

--- maze.c
+++ maze.c
@@ -6,7 +6,6 @@
 #define make_border(x) do {    wborder(x, '|', '|', '-', '-', '+', '+', '+', '+'); } while (0)
 int w_width, w_height;
 int width, height, cmd_width, cmd_height, b_read_in;
-
 WINDOW *play_window, *cmd_window, *start_window;
 
 void save(void)
@@ -21,16 +20,24 @@ void save(void)
 }
 
 void print_start_window()
-{
+{ int y, x;
+  echo();
   start_window = newwin(30, 30, 0, 0);
   make_border(start_window);
-  wprintw(start_window, "MazeMaker v0.1 alpha\n");
-  wprintw(start_window, "By @rjf\n");
-  wprintw(start_window, "terminal size: %dx%d\n", w_width, w_height);
-  wprintw(start_window, "height (0...100): ");
+  getyx(start_window, y, x);
+  ++y; ++x;
+  mvwprintw(start_window, y, x, "MazeMaker v0.1 alpha");
+  ++y;
+  mvwprintw(start_window, y, x, "By @rjf");
+  ++y;
+  mvwprintw(start_window, y, x, "terminal size: %dx%d", w_width, w_height);
+  ++y;
+  mvwprintw(start_window, y, x, "height (0...100): ");
   wscanw(start_window, "%d", &height);
-  wprintw(start_window, "\nwidth (0...100): ");
+  ++y;
+  mvwprintw(start_window, y, x, "width (0...100): ");
   wscanw(start_window, "%d", &width);
+  ++y;
   height = height > 90 ? 90 : height;
   height = height < 10 ? 10 : height;
   width = width > 90 ? 90 : width;
@@ -39,23 +46,44 @@ void print_start_window()
   width = w_width * width / 100;
   cmd_height = w_height - height;
   cmd_width = w_width - width;
-  wprintw(start_window, "width=%d, height=%d, ok?\n", width, height);
+  ++y;
+  mvwprintw(start_window, y, x, "width=%d, height=%d, cmd_width=%d, cmd_height=%d, ok?", width, height, cmd_width, cmd_height );
   wgetch(start_window);
+  noecho();
   delwin(start_window);
+}
+
+void create_main_windows()
+{
+  play_window = newwin(height, width, 0, 0);
+  cmd_window = newwin(cmd_height, cmd_width, 0, width);
+  make_border(play_window);
+  make_border(cmd_window);
+  int cmd_y, cmd_x;
+  getyx(cmd_window, cmd_y, cmd_x);
+  mvwprintw(cmd_window, cmd_y + 1, cmd_x + 1, "[CMD]");
+  wrefresh(play_window);
+  wrefresh(cmd_window);
+}
+
+void free_main_windows()
+{
+  delwin(play_window);
+  delwin(cmd_window);
 }
 
 int main(void)
 {
-  int y = 0, x = 0, do_print = 0;
+  int y = 1, x = 1, do_print = 0;
   char bch, ch, bk = ' ';
   initscr();
-  getmaxyx(stdscr, w_height, w_width); 
   noecho();
+  getmaxyx(stdscr, w_height, w_width); 
   print_start_window();
-
+  create_main_windows();
   while (1) {
-    if (!do_print) mvprintw(y, x, "%c", bch);
-    ch = getch();
+//    if (!do_print) mvwprintw(play_window, y, x, "%c", bch);
+    ch = wgetch(play_window);
     switch (ch) {
       case 'i': do_print = 1 - do_print;
         break;
@@ -64,26 +92,27 @@ int main(void)
       case 'q': 
         goto do_exit;
       case 'w':
-        --y; y = y < 0 ? 0 : y;
+        --y; y = y < 1 ? 1 : y;
         break;
       case 'a':
-        --x; x = x < 0 ? 0 : x;
+        --x; x = x < 1 ? 1 : x;
         break;
       case 's':
-        ++y; y = y > height ? height : y;
+        ++y; y = y > height-2 ? height-2 : y;
         break;
       case 'd':
-        ++x; x = x > width ? width : x;
+        ++x; x = x > width-2 ? width-2 : x;
       break;
       default: bk = ch;
     }
     bch = mvinch(y, x) & A_CHARTEXT;
     if (do_print) {
-      mvprintw(y, x, "%c", bk);
-      refresh();
+      mvwprintw(play_window, y, x, "%c", bk);
+      wrefresh(play_window);
     }
   }
   do_exit:
+  free_main_windows();
   endwin();
   return 0;
 }
