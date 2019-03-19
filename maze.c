b4057f03f24f153f1e819db26ca240bb6d9eaf3d
Rui-Jie Fang <rui.jie.fang@gmail.com>
2019-03-19 15:50:34 -0400

    Initial commit

--- maze.c
+++ maze.c
@@ -3,14 +3,11 @@
 #include <memory.h>
 #include <time.h>
 #include <ncurses.h>
-int width, height, b_read_in;
+#define make_border(x) do {    wborder(x, '|', '|', '-', '-', '+', '+', '+', '+'); } while (0)
+int w_width, w_height;
+int width, height, cmd_width, cmd_height, b_read_in;
 
-/* 
-  Maze generator
-  ----------------
-  > Not yet completed. 
-  > Todo: 
-*/
+WINDOW *play_window, *cmd_window, *start_window;
 
 void save(void)
 {
@@ -23,18 +20,39 @@ void save(void)
   fclose(fp);
 }
 
+void print_start_window()
+{
+  start_window = newwin(30, 30, 0, 0);
+  make_border(start_window);
+  wprintw(start_window, "MazeMaker v0.1 alpha\n");
+  wprintw(start_window, "By @rjf\n");
+  wprintw(start_window, "terminal size: %dx%d\n", w_width, w_height);
+  wprintw(start_window, "height (0...100): ");
+  wscanw(start_window, "%d", &height);
+  wprintw(start_window, "\nwidth (0...100): ");
+  wscanw(start_window, "%d", &width);
+  height = height > 90 ? 90 : height;
+  height = height < 10 ? 10 : height;
+  width = width > 90 ? 90 : width;
+  width = width < 10 ? 10 : width;
+  height = w_height * height / 100;
+  width = w_width * width / 100;
+  cmd_height = w_height - height;
+  cmd_width = w_width - width;
+  wprintw(start_window, "width=%d, height=%d, ok?\n", width, height);
+  wgetch(start_window);
+  delwin(start_window);
+}
+
 int main(void)
 {
   int y = 0, x = 0, do_print = 0;
   char bch, ch, bk = ' ';
-  
-  fputs(">>>>>\tMazeMaker v0.1alpha\t<<<<<\n", stdout);
-  fputs(">>>>>\t\tHeight: ", stdout);
-  scanf("%d", &height);
-  fputs(">>>>>\t\tWidth: ", stdout);
-  scanf("%d", &width);
   initscr();
-  noecho();  
+  getmaxyx(stdscr, w_height, w_width); 
+  noecho();
+  print_start_window();
+
   while (1) {
     if (!do_print) mvprintw(y, x, "%c", bch);
     ch = getch();
